7579a2c37d7099d2d4bcf99d3f0a6f58031928fb
Валерия Борщова <valeria.borshova@gmail.com>
2024-03-24 23:28:55 +0300

    add change-theme

--- public/js/form-processing.js
+++ public/js/form-processing.js
@@ -1,10 +1,20 @@
 const loginInput = document.querySelector("#container-form-content-input-login");
+const passwordInput = document.querySelector("#container-form-content-input-password");
+
+//кнопа что-то не работает
+//const registrationButton = document.getElementById("container-form-content-button");
+
+const button = document.getElementById('button-change-theme');
 
 let inputValueLogin;
+let inputValuePassword;
 
+//registrationButton.setAttribute('disabled', 'disabled');
 loginInput.addEventListener("input", ()=>{
     inputValueLogin = loginInput.value;
-    console.log(inputValueLogin);
+});
+passwordInput.addEventListener("input", ()=>{
+    inputValuePassword = passwordInput.value;
 });
 
 loginInput.addEventListener("blur", ()=>{
@@ -13,7 +23,7 @@ loginInput.addEventListener("blur", ()=>{
         //Мне не понравилось, как выглядит в моей верстке, но оно работает. Запариваться с переделыванием не стала(
         /*const br = document.createElement('br');
         document.querySelector("#errorMessage").append("Incorrect login");*/
-
+        //registrationButton.setAttribute('disabled', 'disabled');
         //поэтому не очень удобный вариант
         alert("Incorrect login");
     }
@@ -22,8 +32,47 @@ loginInput.addEventListener("blur", ()=>{
         loginInput.className="container-form-element input login";
     }
 });
+passwordInput.addEventListener("blur", ()=>{
+    if (!inputValuePassword || inputValuePassword.length<5){
+        passwordInput.className+=" input-error";
+       // registrationButton.setAttribute('disabled', 'disabled');
+        alert("Incorrect password");
+    }
+    else
+    {
+        passwordInput.className="container-form-element input password";
+    }
+});
+
+
 
 //удаление при фокусе
 /*loginInput.addEventListener("focus", ()=>{
     document.getElementById('errorMessage').innerHTML = '';
 });*/
+
+button.addEventListener('click', () => {
+
+    var link = document.getElementById("style-form"),
+        href = link.getAttribute('href');
+
+    if(href == 'public/styles/style.css') {
+        link.href="public/styles/night-style.css";
+    } else if (href == 'public/styles/night-style.css') {
+        link.href="public/styles/style.css";
+    }
+
+});
+
+
+/*registrationButton.addEventListener('click', () => {
+
+    if (inputValuePassword.length>5 && inputValueLogin.length>5)
+    {
+        registrationButton.removeAttribute('disable');
+    }
+
+});*/
+
+
+
